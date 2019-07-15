37345fd844aab119913ceea3ab6bb99a212f17fd
Freddie <f.varnals@gmail.com>
2019-07-15 16:58:36 +0100

    Added test database for RSpec tests

--- spec/pull_from_database_spec.rb
+++ spec/pull_from_database_spec.rb
@@ -2,7 +2,9 @@ require 'pg'
 describe 'pull table from database' do
   it 'pulls bookmarks' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
-    result = connection.exec('SELECT * FROM bookmarks')
-    expect{result}.not_to raise_error
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'www.google.com');")
+    result = connection.exec("SELECT * FROM bookmarks")
+    bookmarks = result.map { |bookmarks| bookmarks['url'] }
+    expect(bookmarks).to include 'www.google.com'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # Set the environment to "test"
-ENV['RACK_ENV'] = 'test'
+require_relative './setup_test_database'
+ENV['ENVIRONMENT'] = 'test'
 
 # Bring in the contents of the `app.rb` file
 require File.join(File.dirname(__FILE__), '..', './lib/app.rb')
@@ -28,6 +29,9 @@ Capybara.app = BookmarkManager
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
