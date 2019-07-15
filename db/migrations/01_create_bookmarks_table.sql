b2a986bebcbb4d0d9de150117b7ec8f1106c53cf
Freddie <f.varnals@gmail.com>
2019-07-15 15:30:51 +0100

    added bookmarks table setup

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,1 +1,7 @@
-CREATE TABLE bookmars(id SERIAL PRIMARY KEY, url VARCHAR(60));
+CREATE DATABASE "bookmark_manager"
+\c bookmark_manager;
+CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+INSERT INTO bookmarks (url) VALUES('www.google.com');
+INSERT INTO bookmarks (url) VALUES('www.makersacademy.com');
+INSERT INTO bookmarks (url) VALUES('www.youtube.com');
+INSERT INTO bookmarks (url) VALUES('www.facebook.com');
