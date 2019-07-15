03f26d44db8061f4aebb5f6487d9347860bdd159
iAquino47 <ianaquino47@gmail.com>
2019-07-15 16:38:09 +0100

    started stage 9

--- spec/pull_from_database_spec.rb
+++ spec/pull_from_database_spec.rb
@@ -1,7 +1,7 @@
 require 'pg'
 describe 'pull table from database' do
   it 'pulls bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager')
+    connection = PG.connect(dbname: 'bookmark_manager_test')
     result = connection.exec('SELECT * FROM bookmarks')
     expect{result}.not_to raise_error
   end
